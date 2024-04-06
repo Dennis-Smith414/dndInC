109f6c5aba07f5cb292969956166942f94612b72
dennis <dennisnsmith2000@gmail.com>
2024-04-06 18:58:59 -0500

    testin to see if player init is working

--- mainGame.C
+++ mainGame.C
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <cstring>
 
 typedef struct {
     char name[10];
@@ -10,37 +11,58 @@ typedef struct {
     int intel;
     int wis;
     int cha;
-    int stealth;
+    bool stealth;
     int magbouns;
     int spellSave;
     int mp;
     int counter;
     int profBouns;
     int percep;
-} Player
+} Player;
 
-void initPlayer(Player* name, double health, int ac, int str,
-int dex, int con, int intel, int wis, int cha, int stealth
-int magbouns, int, spellSave, int mp, int counter, int profBouns
-int percep){
-    strcpy(Player->name, name);
-    Player->ac = ac;
-    Player->str = str;
-    Player->dex = dex;
-    Player->con = con;
-    Player->intel = intel;
-    Player->wis = wis;
-    Player->cha = cha;
-    Player->stealth = stealth;
-    Player->magbouns = magbouns;
-    Player->spellSave = spellSave;
-    Player->mp = mp;
-    Player->counter = counter;
-    Player->profBouns = profBouns;
-    Player->percep = percep;
+void initPlayer(Player* player, double health, int ac, int str, int dex, int con,
+                int intel, int wis, int cha, bool stealth, int magbouns,
+                int spellSave, int mp, int counter, int profBouns, int percep) {
+    strcpy(player->name, "Nathan");
+    player->health = health;
+    player->ac = ac;
+    player->str = str;
+    player->dex = dex;
+    player->con = con;
+    player->intel = intel;
+    player->wis = wis;
+    player->cha = cha;
+    player->stealth = stealth;
+    player->magbouns = magbouns;
+    player->spellSave = spellSave;
+    player->mp = mp;
+    player->counter = counter;
+    player->profBouns = profBouns;
+    player->percep = percep;
 }
 
-int main(){
-    printf("henlo");
+int main() {
+    Player Nathan;
+    initPlayer(&Nathan, 104, 16, 2, 5, 3, 0, 2, 4, false, 0,
+                0, 0, 0, 3, 0);
+
+    printf("Player Stats for Nathan:\n"); 
+    printf("  Name: %s\n", Nathan.name);
+    printf("  Health: %.2lf\n", Nathan.health); 
+    printf("  AC: %d\n", Nathan.ac);
+    printf("  Strength: %d\n", Nathan.str);
+    printf("  Dexterity: %d\n", Nathan.dex);
+    printf("  Constitution: %d\n", Nathan.con);
+    printf("  Intelligence: %d\n", Nathan.intel);
+    printf("  Wisdom: %d\n", Nathan.wis);
+    printf("  Charisma: %d\n", Nathan.cha);
+    printf("  Stealth: %s\n", Nathan.stealth ? "True" : "False");
+    printf("  Magic Bonus: %d\n", Nathan.magbouns);
+    printf("  Spell Save: %d\n", Nathan.spellSave);
+    printf("  Mana Points: %d\n", Nathan.mp);
+    printf("  Counter: %d\n", Nathan.counter);
+    printf("  Proficiency Bonus: %d\n", Nathan.profBouns);
+    printf("  Perception: %d\n", Nathan.percep);
+
     return 0;
 }
