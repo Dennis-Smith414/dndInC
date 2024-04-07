2b732843fdf9dae606ba8359c9bcb2b820d4af8a
dennis <dennisnsmith2000@gmail.com>
2024-04-06 19:25:18 -0500

    Tested structs for all players by printing charecter stats on screen

--- mainGame.C
+++ mainGame.C
@@ -43,8 +43,19 @@ void initPlayer(Player* player, double health, int ac, int str, int dex, int con
 
 int main() {
     Player Nathan;
+    Player Litrix;
+    Player Albrich;
+    Player Etranth;
     initPlayer(&Nathan, 104, 16, 2, 5, 3, 0, 2, 4, false, 0,
                 0, 0, 0, 3, 0);
+    initPlayer(&Litrix, 115, 18, 1, 4, 4, 1, 1, 0, false, 0, 
+                0, 0, 0, 3, 0);
+
+    initPlayer(&Albrich, 98, 15, -1, 2, 4, 0, 0, 5, false, 6,
+                15, 30, 0, 3, 0);
+
+    initPlayer(&Etranth, 300, 10, 5, 0, 4, 2, 1, 4, false, 0,
+                0, 0, 0, 0, 13);
 
     printf("Player Stats for Nathan:\n"); 
     printf("  Name: %s\n", Nathan.name);
@@ -63,6 +74,60 @@ int main() {
     printf("  Counter: %d\n", Nathan.counter);
     printf("  Proficiency Bonus: %d\n", Nathan.profBouns);
     printf("  Perception: %d\n", Nathan.percep);
+
+    printf("\nPlayer Stats for Litrix:\n"); 
+    printf("  Name: %s\n", Litrix.name);
+    printf("  Health: %.2lf\n", Litrix.health); 
+    printf("  AC: %d\n", Litrix.ac);
+    printf("  Strength: %d\n", Litrix.str);
+    printf("  Dexterity: %d\n", Litrix.dex);
+    printf("  Constitution: %d\n", Litrix.con);
+    printf("  Intelligence: %d\n", Litrix.intel);
+    printf("  Wisdom: %d\n", Litrix.wis);
+    printf("  Charisma: %d\n", Litrix.cha);
+    printf("  Stealth: %s\n", Litrix.stealth ? "True" : "False");
+    printf("  Magic Bonus: %d\n", Litrix.magbouns);
+    printf("  Spell Save: %d\n", Litrix.spellSave);
+    printf("  Mana Points: %d\n", Litrix.mp);
+    printf("  Counter: %d\n", Litrix.counter);
+    printf("  Proficiency Bonus: %d\n", Litrix.profBouns);
+    printf("  Perception: %d\n", Litrix.percep);
+
+    printf("\nPlayer Stats for Albrich:\n"); 
+    printf("  Name: %s\n", Albrich.name);
+    printf("  Health: %.2lf\n", Albrich.health); 
+    printf("  AC: %d\n", Albrich.ac);
+    printf("  Strength: %d\n", Albrich.str);
+    printf("  Dexterity: %d\n", Albrich.dex);
+    printf("  Constitution: %d\n", Albrich.con);
+    printf("  Intelligence: %d\n", Albrich.intel);
+    printf("  Wisdom: %d\n", Albrich.wis);
+    printf("  Charisma: %d\n", Albrich.cha);
+    printf("  Stealth: %s\n", Albrich.stealth ? "True" : "False");
+    printf("  Magic Bonus: %d\n", Albrich.magbouns);
+    printf("  Spell Save: %d\n", Albrich.spellSave);
+    printf("  Mana Points: %d\n", Albrich.mp);
+    printf("  Counter: %d\n", Albrich.counter);
+    printf("  Proficiency Bonus: %d\n", Albrich.profBouns);
+    printf("  Perception: %d\n", Albrich.percep);
+
+    printf("\nPlayer Stats for Etranth:\n"); 
+    printf("  Name: %s\n", Etranth.name);
+    printf("  Health: %.2lf\n", Etranth.health); 
+    printf("  AC: %d\n", Etranth.ac);
+    printf("  Strength: %d\n", Etranth.str);
+    printf("  Dexterity: %d\n", Etranth.dex);
+    printf("  Constitution: %d\n", Etranth.con);
+    printf("  Intelligence: %d\n", Etranth.intel);
+    printf("  Wisdom: %d\n", Etranth.wis);
+    printf("  Charisma: %d\n", Etranth.cha);
+    printf("  Stealth: %s\n", Etranth.stealth ? "True" : "False");
+    printf("  Magic Bonus: %d\n", Etranth.magbouns);
+    printf("  Spell Save: %d\n", Etranth.spellSave);
+    printf("  Mana Points: %d\n", Etranth.mp);
+    printf("  Counter: %d\n", Etranth.counter);
+    printf("  Proficiency Bonus: %d\n", Etranth.profBouns);
+    printf("  Perception: %d\n", Etranth.percep);
 
     return 0;
 }
